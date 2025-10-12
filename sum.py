e8719f80619331c01784c3e400088ddb7341833c
psam-717 <155116265+psam-717@users.noreply.github.com>
2025-10-12 11:21:16 +0000

    docs: illustrate basic statistical operations on lists using built-in functions

---
+++ sum.py
@@ -0,0 +1,19 @@
+nums_arr = [15, 1,2,3,4,5,10]
+
+# sum of num list
+sum_of_nums = sum(nums_arr)
+
+# maximum number
+max_num = max(nums_arr)
+
+
+# minimum number
+min_num = min(nums_arr)
+
+# count of numbers in list
+count = len(nums_arr)
+
+# average
+average = sum_of_nums / count
+print(average)
+
