b995aaddf4c83a79a2c8e41d5fdfcadb7e83e327
psam-717 <155116265+psam-717@users.noreply.github.com>
2025-10-12 11:20:47 +0000

    docs: demonstrate list sorting techniques with ascending and descending orders

---
+++ sorted.py
@@ -0,0 +1,15 @@
+nums = [8,4,5,1,3]
+
+# in ascending order 
+nums.sort()
+
+# descending order
+nums.sort(reverse=True)
+print(nums)
+
+# ascending order
+ascending = sorted(nums)
+
+# descending order
+descending = sorted(nums, reverse=True)
+print(descending)
