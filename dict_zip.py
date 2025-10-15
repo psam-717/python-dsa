5dbe82fbff596ce33142817de75cfd79177cc471
psam-717 <155116265+psam-717@users.noreply.github.com>
2025-10-15 10:33:54 +0000

    feat: add Python example for creating dictionary with zip function

---
+++ dict_zip.py
@@ -0,0 +1,7 @@
+ids = [1,2,3,4,5,6]
+user_names = ['psam', 'maps', 'zap', 'amps', 'marv', 'phil']
+
+# a dictionary of ids and user names
+id_user_dict = dict(zip(ids, user_names))
+
+print(id_user_dict)
