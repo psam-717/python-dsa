b287c93baf948543fe86dfda43d59510cb14e3bf
psam-717 <155116265+psam-717@users.noreply.github.com>
2025-12-06 23:22:42 +0000

    no major updates

--- dict_zip.py
+++ dict_zip.py
@@ -4,4 +4,4 @@ user_names = ['psam', 'maps', 'zap', 'amps', 'marv', 'phil']
 # a dictionary of ids and user names
 id_user_dict = dict(zip(ids, user_names))
 
-print(id_user_dict)
+print(id_user_dict) 
