234e9c1f042a75c3ae8c8c09c69be3db07c53783
psam-717 <155116265+psam-717@users.noreply.github.com>
2026-01-31 23:45:48 +0000

    just comments

--- main.py
+++ main.py
@@ -5,4 +5,5 @@ def greet_user(name: str):
     print(f'{name.capitalize()}, welcome to our python tutorials')
     print(f'Hey there {name.capitalize()}')
 
-greet_user('marvin')   
+greet_user('marvin')
+   
